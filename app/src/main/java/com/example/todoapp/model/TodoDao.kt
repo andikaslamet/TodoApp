333d5807c6d924a6655544ed0a6b64b55272095a
acer <s160421085@student.ubaya.ac.id>
2024-05-16 15:32:35 +0700

    week 8

---
+++ app/src/main/java/com/example/todoapp/model/TodoDao.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg todo:Todo)
+
+    @Query("select * from todo")
+    fun selectAllTodo(): List<Todo>
+
+    @Query("select * from todo where uuid= :id")
+    fun selectTodo(id:Int): Todo
+
+    @Delete
+    fun deleteTodo(todo:Todo)
+}
---
+++ app/src/main/java/com/example/todoapp/view/TodoListFragment.kt
@@ -0,0 +1,70 @@
+package com.example.todoapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentTodoListBinding
+import com.example.todoapp.viewmodel.ListTodoViewModel
+
+
+class TodoListFragment : Fragment() {
+    private lateinit var viewModel: ListTodoViewModel
+    private val todoListAdapter  = TodoListAdapter(arrayListOf())
+    private lateinit var binding: FragmentTodoListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTodoListBinding.inflate(inflater,container,false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListTodoViewModel::class.java)
+        viewModel.refresh()
+        binding.recViewToDo.layoutManager = LinearLayoutManager(context)
+        binding.recViewToDo.adapter = todoListAdapter
+        binding.btnFab.setOnClickListener {
+            val action = TodoListFragmentDirections.actionCreateTodo()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        observeViewModel()
+
+    }
+    fun observeViewModel() {
+        viewModel.todoLD.observe(viewLifecycleOwner, Observer {
+            todoListAdapter.updateTodoList(it)
+            if(it.isEmpty()) {
+                binding.recViewToDo?.visibility = View.GONE
+                binding.txtError.setText("Your todo still empty.")
+            } else {
+                binding.recViewToDo?.visibility = View.VISIBLE
+            }
+            viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+                if(it == false) {
+                    binding.progressBar?.visibility = View.GONE
+                } else {
+                    binding.progressBar?.visibility = View.VISIBLE
+                }
+            })
+            viewModel.todoLoadErrorLD.observe(viewLifecycleOwner, Observer {
+                if(it == false) {
+                    binding.txtError?.visibility = View.GONE
+                } else {
+                    binding.txtError?.visibility = View.VISIBLE
+                }
+            })
+        })
+
+    }
+
+}
